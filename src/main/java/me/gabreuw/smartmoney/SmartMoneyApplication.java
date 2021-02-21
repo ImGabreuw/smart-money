7efb83b37db63cdc179e7d836bceaae57fa5047b
ImGabreuw <imgabreuw.dev@gmail.com>
2021-02-21 11:00:05 -0300

    Projeto criado

---
+++ src/main/java/me/gabreuw/smartmoney/SmartMoneyApplication.java
@@ -0,0 +1,13 @@
+package me.gabreuw.smartmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gabreuw/smartmoney/SmartMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.smartmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
